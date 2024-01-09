b4469041ce2e98e9beba82fc278e73be7864e0a8
Vishnu Vardhan Gowd Vaka <vishnuvardhanvaka1@gmail.com>
2024-01-09 10:35:21 +0530

    Update Chat.jsx

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -78,7 +78,7 @@ function Chat({ URL }) {
       },
       body: form
     }
-    console.log(URL + 'chat/', fetchReq,'****************')
+    console.log(URL,URL+'chat/', fetchReq,'****************')
     const response = await fetch(URL + 'chat/', fetchReq)
     const status_code = response.status
     const data = await response.json()
